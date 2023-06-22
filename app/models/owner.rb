631587a69478e8b407bee1b812e022f86c0729d6
William Ugalde Gamboa <wugalde@ltvco.com>
2023-06-21 22:09:57 -0600

    Add association between Person and Pet

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -4,4 +4,7 @@ class Owner < ApplicationRecord
   validates :name, :email, :age, presence: true
   validates :email, uniqueness: true
   validates_format_of :email, with: /\A\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+\z/
+
+  has_many :pets, dependent: :destroy
+
 end
