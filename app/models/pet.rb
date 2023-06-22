429360bede0212ed50d6fe0aa398277248c25dd1
William Ugalde Gamboa <wugalde@ltvco.com>
2023-06-21 22:30:23 -0600

    Add instance methods to Pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -6,4 +6,9 @@ class Pet < ApplicationRecord
   belongs_to :owner
   belongs_to :animal
 
+  def days_since_adopted
+    return 0 if adoption_date.nil?
+    (Time.now - adoption_date) / 1.day
+  end
+
 end
