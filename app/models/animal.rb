2d785e65dc0a94397983fe3a9625650cefe919ef
William Ugalde Gamboa <wugalde@ltvco.com>
2023-06-21 22:15:16 -0600

    Add scopes to Animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,9 @@
 class Animal < ApplicationRecord
   self.table_name = 'animal'
 
+  scope :dogs_only, -> { where(species: 'dog') }
+  scope :cats_only, -> { where(species: 'cat') }
+
   validates :age, :color, :weight, :species, presence: true
 
   has_many :pets
