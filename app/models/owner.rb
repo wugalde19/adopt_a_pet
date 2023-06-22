6ac9b8f762915ca6e022a4bf75e18af9b4538bda
William Ugalde Gamboa <wugalde@ltvco.com>
2023-06-21 22:08:53 -0600

    Add validations to Owner model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,7 @@
 class Owner < ApplicationRecord
   self.table_name = 'owner'
 
+  validates :name, :email, :age, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+\z/
 end
