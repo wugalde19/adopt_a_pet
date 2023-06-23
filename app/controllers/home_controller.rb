c4f69a59aad0baad101855da964690bd08a858d1
William Ugalde Gamboa <wugalde@ltvco.com>
2023-06-22 22:48:30 -0600

    Create /about route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
     render html: 'Main Page'
   end
+
+  def about
+    render html: 'About Page'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  get "/about", to: "home#about"
 end
